77c4974cc63110d6e2bb57aa94933aced048141e
Adam Styer <adam.styer8@gmail.com>
2018-10-11 23:01:16 -0400

    Finished sprint 1 prototype

--- app/src/main/java/com/example/als1543/myapplication/MainActivity.java
+++ app/src/main/java/com/example/als1543/myapplication/MainActivity.java
@@ -1,7 +1,11 @@
 package com.example.als1543.myapplication;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,24 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        TextView humanCount = findViewById(R.id.humanCount);
+        humanCount.setText("452");
+        TextView zombieCount = findViewById(R.id.zombieCount);
+        zombieCount.setText("170");
+
+        goToMissions();
+    }
+
+    public void goToMissions() {
+        Button toMissions = (Button) findViewById(R.id.toMissionsButton);
+        toMissions.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View view)
+            {
+                startActivity(new Intent(MainActivity.this, Missions.class));
+            }
+        });
     }
 }
--- app/src/main/java/com/example/als1543/myapplication/Missions.java
+++ app/src/main/java/com/example/als1543/myapplication/Missions.java
@@ -2,6 +2,7 @@ package com.example.als1543.myapplication;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class Missions extends AppCompatActivity {
 
@@ -9,5 +10,23 @@ public class Missions extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_missions);
+
+        TextView humanCount = findViewById(R.id.humanCount);
+        humanCount.setText("452");
+        TextView zombieCount = findViewById(R.id.zombieCount);
+        zombieCount.setText("170");
+
+        TextView fridayText = findViewById(R.id.fridayText);
+        fridayText.setText("Midday Mission: Description\nNight Mission: Description\nStory: Story text");
+        TextView thursText = findViewById(R.id.thursdayText);
+        thursText.setText("Midday Mission: Description\nNight Mission: Description\nStory: Story text");
+        TextView wedText = findViewById(R.id.wednesdayText);
+        wedText.setText("Midday Mission: Description\nNight Mission: Description\nStory: Story text");
+        TextView tuesText = findViewById(R.id.tuesdayText);
+        tuesText.setText("Midday Mission: Description\nNight Mission: Description\nStory: Story text");
+        TextView monText = findViewById(R.id.mondayText);
+        monText.setText("Midday Mission: Description\nNight Mission: Description\nStory: Story text");
+        TextView sunText = findViewById(R.id.sundayText);
+        sunText.setText("Midday Mission: Description\nNight Mission: Description\nStory: Story text");
     }
 }
