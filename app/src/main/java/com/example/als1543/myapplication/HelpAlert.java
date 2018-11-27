bea7ccf9f2c57ec963fafb5c60ebf15bac4ea3aa
Adam Styer <adam.styer8@gmail.com>
2018-11-27 14:46:34 -0500

    help button changes when pressed

--- app/src/main/java/com/example/als1543/myapplication/HelpAlert.java
+++ app/src/main/java/com/example/als1543/myapplication/HelpAlert.java
@@ -1,7 +1,10 @@
 package com.example.als1543.myapplication;
 
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class HelpAlert extends AppCompatActivity {
 
@@ -9,5 +12,20 @@ public class HelpAlert extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_help_alert);
+
+        sendButtonPress();
+    }
+
+    public void sendButtonPress() {
+        final Button sendButton = (Button) findViewById(R.id.SendButton);
+        sendButton.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View view)
+            {
+                sendButton.setText("SENT");
+                sendButton.setBackgroundColor(Color.GREEN);
+            }
+        });
     }
 }
